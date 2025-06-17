37881ffdbf19c7fb988f67a10d2045032c355fef
thomas-nurse <thomas.p.nurse@gmail.com>
2025-06-17 16:30:24 +0100

    Update Comments

--- src/main/java/org/apiary/spawningindustry/block/SIBlocks.java
+++ src/main/java/org/apiary/spawningindustry/block/SIBlocks.java
@@ -12,14 +12,14 @@ import org.apiary.spawningindustry.block.kinetic.BrassMechanistSpawnerBlock;
 import org.apiary.spawningindustry.main.SIConstants;
 
 public class SIBlocks {
-    // Deferred Register for blocks under the mod's namespace.
+    // Deferred Register for blocks.
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(SIConstants.MODID);
 
-    //Haunted Iron Blocks
+    //Haunted Iron Blocks.
     public static final DeferredBlock<Block> HAUNTED_IRON_BLOCK =
             BLOCKS.registerSimpleBlock("haunted_iron_block", BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BLOCK).mapColor(MapColor.TERRACOTTA_CYAN));
 
-    // Register the Andesite Mechanist Spawner Block.
+    // Andesite Mechanist Spawner Block.
     public static final DeferredHolder<Block, AndesiteMechanistSpawnerBlock> ANDESITE_MECHANIST_SPAWNER_BLOCK =
             BLOCKS.register("andesite_mechanist_spawner", () -> new AndesiteMechanistSpawnerBlock(BlockBehaviour.Properties.of()
                             .mapColor(MapColor.STONE)
@@ -27,7 +27,7 @@ public class SIBlocks {
                             .requiresCorrectToolForDrops()
                             .noOcclusion()));
 
-    // Register the Brass Mechanist Spawner Block.
+    // Brass Mechanist Spawner Block.
     public static final DeferredHolder<Block, BrassMechanistSpawnerBlock> BRASS_MECHANIST_SPAWNER_BLOCK =
             BLOCKS.register("brass_mechanist_spawner", () -> new BrassMechanistSpawnerBlock(BlockBehaviour.Properties.of()
                             .mapColor(MapColor.METAL)
