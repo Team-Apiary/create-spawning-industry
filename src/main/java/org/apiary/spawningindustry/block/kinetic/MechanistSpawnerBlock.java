77136d510ea385aab4dbff4045b4582fbecfb53e
thomas-nurse <thomas.p.nurse@gmail.com>
2025-06-18 09:26:43 +0100

    Add removing a Soulbound Nexus from Mechanist Spawners

--- src/main/java/org/apiary/spawningindustry/block/kinetic/MechanistSpawnerBlock.java
+++ src/main/java/org/apiary/spawningindustry/block/kinetic/MechanistSpawnerBlock.java
@@ -5,22 +5,71 @@ import com.simibubi.create.content.kinetics.base.IRotate;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.Direction.Axis;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import com.simibubi.create.foundation.block.IBE;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import org.apiary.spawningindustry.component.SIDataComponents;
 import org.apiary.spawningindustry.entity.block.kinetic.MechanistSpawnerBlockEntity;
+import org.apiary.spawningindustry.item.SIItems;
 import org.jetbrains.annotations.NotNull;
 
-public abstract class MechanistSpawnerBlock<T extends MechanistSpawnerBlockEntity>
-        extends HorizontalKineticBlock implements IBE<T>, IRotate {
+public abstract class MechanistSpawnerBlock<T extends MechanistSpawnerBlockEntity> extends HorizontalKineticBlock implements IBE<T>, IRotate {
 
     public MechanistSpawnerBlock(BlockBehaviour.Properties properties) {
         super(properties);
+    }
+
+    //Give the player a Soulbound Nexus when they crouch-right-click the spawner, remove the bound soul and transfer it to the given nexus
+    @Override
+    protected @NotNull InteractionResult useWithoutItem(@NotNull BlockState state, Level level, @NotNull BlockPos pos, @NotNull Player player, @NotNull BlockHitResult hitResult) {
+        if (level.isClientSide()) {
+            return InteractionResult.SUCCESS;
+        }
+
+        if (!player.isCrouching()) {
+            return super.useWithoutItem(state, level, pos, player, hitResult);
+        }
+
+        BlockEntity be = level.getBlockEntity(pos);
+        if (!(be instanceof MechanistSpawnerBlockEntity spawner)) {
+            return super.useWithoutItem(state, level, pos, player, hitResult);
+        }
+
+        ResourceLocation boundEntity = spawner.getEntityType();
+        if (boundEntity == null) {
+            return super.useWithoutItem(state, level, pos, player, hitResult);
+        }
+
+        ItemStack nexusStack = new ItemStack(SIItems.SOULBOUND_NEXUS.get());
+        nexusStack.set(SIDataComponents.ENTITY_TYPE, boundEntity);
+
+        spawner.setEntityType(null);
+        spawner.setChanged();
+        level.sendBlockUpdated(pos, state, state, 3);
+
+        if (!player.getInventory().add(nexusStack)) {
+            ItemEntity dropped = new ItemEntity(level,
+                    pos.getX() + 0.5,
+                    pos.getY() + 1.0,
+                    pos.getZ() + 0.5,
+                    nexusStack);
+            level.addFreshEntity(dropped);
+        }
+
+        return InteractionResult.SUCCESS;
     }
 
     // Accept a shaft only from the bottom.
